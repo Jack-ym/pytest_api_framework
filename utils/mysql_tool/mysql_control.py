f51dcc7db5315f36410c5b3503ee09e4473150cb
runyu <15168227013@163.com>
2023-08-11 17:51:07 +0800

    提交mysql tool，addrss_detection

---
+++ utils/mysql_tool/mysql_control.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time   : 2023/8/11 14:58
+@Author : zhaiyanming
+"""
---
+++ utils/other_tools/address_detection.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time   : 2023/8/11 14:57
+@Author : zhaiyanming
+"""
